8a86411bffa656e67e219e40baa177d8ee0769f5
rawillia9801 <163735923+rawillia9801@users.noreply.github.com>
2026-04-28 04:20:43 -0400

    Finalize live mount: upgrade weather station auth

--- lib/weather.ts
+++ lib/weather.ts
@@ -2,9 +2,14 @@ import axios from 'axios'
 
 const API_KEY = process.env.WEATHER_API_KEY
 const STATION_ID = process.env.STATION_ID
+const STATION_KEY = process.env.STATION_KEY
 
 export async function fetchCurrentStationWeather() {
-  const url = `https://api.weather.com/v2/pws/observations/current?stationId=${STATION_ID}&format=json&units=e&apiKey=${API_KEY}`
-  const { data } = await axios.get(url)
+  const url = `https://api.weather.com/v2/pws/observations/current?stationId=${STATION_ID}&format=json&units=e&numericPrecision=decimal&apiKey=${API_KEY}`
+  const { data } = await axios.get(url, {
+    headers: {
+      'X-Station-Key': STATION_KEY || ''
+    }
+  })
   return data.observations?.[0] || null
 }
