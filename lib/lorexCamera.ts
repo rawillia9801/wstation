147ca0e3d7dbd47a086a5142e55fc8a9c0cfd5a2
rawillia9801 <163735923+rawillia9801@users.noreply.github.com>
2026-05-04 01:59:34 -0400

    Expand Lorex camera client helper

--- lib/lorexCamera.ts
+++ lib/lorexCamera.ts
@@ -3,3 +3,9 @@ export const LOREX_CAMERA_ENDPOINT = '/api/camera-feed'
 export function lorexCameraUrl() {
   return `${LOREX_CAMERA_ENDPOINT}?ts=${Date.now()}`
 }
+
+export function nextLorexCameraUrl(refreshKey: number) {
+  return `${LOREX_CAMERA_ENDPOINT}?ts=${refreshKey}`
+}
+
+export const lorexCameraLabel = 'Cars Camera'
